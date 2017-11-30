6368758fee56e8d0521405c7a63dab923cae3d36
Renan Lalier <renanc@ciandt.com>
2017-11-30 16:52:09 -0200

    Commit inicial

--- app/register/register.controller.js
+++ app/register/register.controller.js
@@ -1,9 +1,23 @@
-angular.module('app').controller('RegisterController', RegisterController);
+(function() {
+  'use strict';
+  angular.module('app').controller('RegisterController', RegisterController);
 
-function RegisterController() {
+  RegisterController.$inject = ['validationService'];
+
+  function RegisterController(validationService) {
 
     var vm = this;
+    vm.validate = validate;
 
-    vm.message = "Welcome Test Unit with Jasmine and karmaJS"
+    vm.message = "Welcome Test Unit with Jasmine and karmaJS";
+    vm.showResult = false;
 
-}
+    function validate() {
+      vm.isValidCPF = validationService.validateCpf(vm.cpf);
+      vm.isValidEmail = validationService.validateEmail(vm.email);
+      vm.showResult = true;
+    }
+
+  }
+
+})();
--- app/services/validation.service.js
+++ app/services/validation.service.js
@@ -1,38 +1,40 @@
-angular.module('app').factory('validationService', validationService);
+(function() {
 
-function validationService() {
+  angular.module('app').factory('validationService', validationService);
+
+  function validationService() {
 
     var service = {
-        validateCpf: validateCpf,
-        validateEmail: validateEmail
+      validateCpf: validateCpf,
+      validateEmail: validateEmail
     };
 
     return service;
 
     function validateCpf(strCPF) {
-        var soma;
-        var resto;
-        soma = 0;
-        if (strCPF == "00000000000") return false;
+      var soma;
+      var resto;
+      soma = 0;
+      if (strCPF == "00000000000") return false;
 
-        for (i = 1; i <= 9; i++) soma = soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
-        resto = (soma * 10) % 11;
+      for (i = 1; i <= 9; i++) soma = soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+      resto = (soma * 10) % 11;
 
-        if ((resto == 10) || (resto == 11)) resto = 0;
-        if (resto != parseInt(strCPF.substring(9, 10))) return false;
+      if ((resto == 10) || (resto == 11)) resto = 0;
+      if (resto != parseInt(strCPF.substring(9, 10))) return false;
 
-        soma = 0;
-        for (i = 1; i <= 10; i++) soma = soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
-        resto = (soma * 10) % 11;
+      soma = 0;
+      for (i = 1; i <= 10; i++) soma = soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+      resto = (soma * 10) % 11;
 
-        if ((resto == 10) || (resto == 11)) resto = 0;
-        if (resto != parseInt(strCPF.substring(10, 11))) return false;
-        return true;
+      if ((resto == 10) || (resto == 11)) resto = 0;
+      if (resto != parseInt(strCPF.substring(10, 11))) return false;
+      return true;
     }
 
     function validateEmail(email) {
-        var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        return regex.test(email);
+      var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return regex.test(email);
     };
-
-}
+  }
+})();
